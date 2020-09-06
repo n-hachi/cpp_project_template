d147224d4887557df6eeaf7131e529bbe40ab01f
Nozomu Hatta <nozomuhatta@gmail.com>
2020-09-06 23:02:44 +0900

    Add required positional argument

--- app/main.cpp
+++ app/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <string>
 
 #include "CLI/CLI.hpp"
 
 #include "mylib/mylib.hpp"
 
 int main(int argc, char *argv[]) {
-  CLI::App app;
+  CLI::App app("Calculate fibonacci", argv[0]);
+
+  unsigned int index_;
+  app.add_option("index", index_,
+                 "The number of the number in the fibonacci sequence")
+      ->required();
+
   CLI11_PARSE(app, argc, argv);
+
+  std::cout << mylib::Fibonacci(index_) << std::endl;
+
   return 0;
 }
